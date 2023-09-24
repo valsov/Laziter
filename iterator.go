bb74678872b5e74936f939b9649434cc58618dac
Valsov <valentin.sovignet@epsi.fr>
2023-09-24 19:28:48 +0200

    Base implementation using channels

--- iterator.go
+++ iterator.go
@@ -1,36 +1,39 @@
-package main
+package laziter
 
 type Iterator[T any] interface {
 	NextValue() (T, bool)
 	Next() bool
 	GetCurrentValue() (T, bool)
-	Reset() // todo: need index
+	ResetIteratorPosition()
 	GetValuesProvider() ValuesProvider[T]
+	Close()
 }
 
 type ValuesProvider[T any] interface {
-	Yield(value T) bool
+	Wait() bool
+	Yield(value T)
 	Close()
+}
+
+type PersitableIterator[T any] struct {
+	Provider      *BaseValuesProvider[T]
+	InternalSlice []T
+	currentIndex  int
+	currentValue  T
+	hasValue      bool
+	persistValues bool
 }
 
 func New[T any](persistValues bool) Iterator[T] {
 	return &PersitableIterator[T]{
 		InternalSlice: []T{},
 		persistValues: persistValues,
+		currentIndex:  -1,
 		Provider: &BaseValuesProvider[T]{
-			valuesChannel: make(chan T),
+			valuesChannel: make(chan T, 1),
 			nextChannel:   make(chan struct{}),
-			quit:          make(chan struct{}),
 		},
 	}
-}
-
-type PersitableIterator[T any] struct {
-	Provider      *BaseValuesProvider[T]
-	InternalSlice []T
-	persistValues bool
-	currentValue  T
-	hasValue      bool
 }
 
 func (i *PersitableIterator[T]) NextValue() (T, bool) {
@@ -46,20 +49,26 @@ func (i *PersitableIterator[T]) Next() bool {
 	ok := true
 
 	select {
-	case <-i.Provider.quit:
-		return false
 	case value, ok = <-i.Provider.valuesChannel:
 		// Value already available or closed chan
 	case i.Provider.nextChannel <- struct{}{}: // Send Next value signal
 		value, ok = <-i.Provider.valuesChannel
 	}
 
+	i.hasValue = true
 	if ok {
-		i.hasValue = true
 		i.currentValue = value
 		if i.persistValues {
 			i.store(value)
+			i.currentIndex++
 		}
+	} else if i.persistValues && len(i.InternalSlice) > i.currentIndex+1 {
+		// In the case of a reset with stored values
+		i.currentIndex++
+		i.currentValue = i.InternalSlice[i.currentIndex]
+		ok = true
+	} else {
+		i.hasValue = false
 	}
 	return ok
 }
@@ -72,8 +81,16 @@ func (i *PersitableIterator[T]) GetCurrentValue() (T, bool) {
 	return defaultVal, false
 }
 
+func (i *PersitableIterator[T]) ResetIteratorPosition() {
+	i.currentIndex = -1
+}
+
 func (i *PersitableIterator[T]) GetValuesProvider() ValuesProvider[T] {
 	return i.Provider
+}
+
+func (i *PersitableIterator[T]) Close() {
+	close(i.Provider.nextChannel)
 }
 
 func (i *PersitableIterator[T]) store(value T) {
@@ -81,21 +98,19 @@ func (i *PersitableIterator[T]) store(value T) {
 }
 
 type BaseValuesProvider[T any] struct {
-	valuesChannel     chan T
-	nextChannel, quit chan struct{}
+	valuesChannel chan T
+	nextChannel   chan struct{}
 }
 
-func (vp *BaseValuesProvider[T]) Yield(value T) bool {
-	select {
-	case <-vp.nextChannel:
-		vp.valuesChannel <- value
-		return true
-	case <-vp.quit:
-		return false
-	}
+func (vp *BaseValuesProvider[T]) Wait() bool {
+	_, ok := <-vp.nextChannel
+	return ok
+}
+
+func (vp *BaseValuesProvider[T]) Yield(value T) {
+	vp.valuesChannel <- value
 }
 
 func (vp *BaseValuesProvider[T]) Close() {
 	close(vp.valuesChannel)
-	// todo: Quit ?
 }
