f12269d9a57169d53e08f366915569e3a137b714
Valsov <valentin.sovignet@sfr.fr>
2023-10-15 08:53:09 +0200

    Support ResetIteratorPosition() while iterating

--- iterator.go
+++ iterator.go
@@ -1,15 +1,25 @@
 package laziter
 
+// Allows value retrieval at each iteration
 type Iterator[T any] interface {
+	// Try to request the next value
 	NextValue() (T, bool)
+	// Try to request the next value, if it succeeds it is stored for later retrieval
 	Next() bool
+	// Retrieve the current iterator internal value if it exists
 	GetCurrentValue() (T, bool)
+	// Reset the current persisted values index
 	ResetIteratorPosition()
+	// Get the ValuesProvider associated with the Iterator
 	GetValuesProvider() *ValuesProvider[T]
+	// Stop the iteration process. If the value provider is still able to produce values, it should stop doing so as the transport channel is now closed.
+	//
+	// This method should always be called after the iteration is no longer needed to prevent leaking goroutines. ('defer' is useful here)
 	Close()
 }
 
-type PersitableIterator[T any] struct {
+// Default iterator implementation. Allows persisting iterated values.
+type persitableIterator[T any] struct {
 	Provider      *ValuesProvider[T]
 	InternalSlice []T
 	currentIndex  int
@@ -18,8 +28,9 @@ type PersitableIterator[T any] struct {
 	persistValues bool
 }
 
+// Produce a configured iterator with a ValuesProvider
 func New[T any](persistValues bool) Iterator[T] {
-	return &PersitableIterator[T]{
+	return &persitableIterator[T]{
 		InternalSlice: []T{},
 		persistValues: persistValues,
 		currentIndex:  -1,
@@ -30,7 +41,7 @@ func New[T any](persistValues bool) Iterator[T] {
 	}
 }
 
-func (i *PersitableIterator[T]) NextValue() (T, bool) {
+func (i *persitableIterator[T]) NextValue() (T, bool) {
 	if !i.Next() {
 		var value T
 		return value, false
@@ -38,7 +49,15 @@ func (i *PersitableIterator[T]) NextValue() (T, bool) {
 	return i.GetCurrentValue()
 }
 
-func (i *PersitableIterator[T]) Next() bool {
+func (i *persitableIterator[T]) Next() bool {
+	// Check if the currentIndex is within already available values (case of a currentIndex reset)
+	if i.persistValues && len(i.InternalSlice) > i.currentIndex+1 {
+		i.currentIndex++
+		i.hasValue = true
+		i.currentValue = i.InternalSlice[i.currentIndex]
+		return true
+	}
+
 	var (
 		value T
 		ok    bool
@@ -51,25 +70,18 @@ func (i *PersitableIterator[T]) Next() bool {
 		value, ok = <-i.Provider.valuesChannel
 	}
 
-	i.hasValue = true
+	i.hasValue = ok
 	if ok {
 		i.currentValue = value
 		if i.persistValues {
 			i.store(value)
 			i.currentIndex++
 		}
-	} else if i.persistValues && len(i.InternalSlice) > i.currentIndex+1 {
-		// In the case of a reset with stored values
-		i.currentIndex++
-		i.currentValue = i.InternalSlice[i.currentIndex]
-		ok = true
-	} else {
-		i.hasValue = false
 	}
 	return ok
 }
 
-func (i *PersitableIterator[T]) GetCurrentValue() (T, bool) {
+func (i *persitableIterator[T]) GetCurrentValue() (T, bool) {
 	if i.hasValue {
 		return i.currentValue, true
 	}
@@ -77,18 +89,19 @@ func (i *PersitableIterator[T]) GetCurrentValue() (T, bool) {
 	return defaultVal, false
 }
 
-func (i *PersitableIterator[T]) ResetIteratorPosition() {
+func (i *persitableIterator[T]) ResetIteratorPosition() {
 	i.currentIndex = -1
 }
 
-func (i *PersitableIterator[T]) GetValuesProvider() *ValuesProvider[T] {
+func (i *persitableIterator[T]) GetValuesProvider() *ValuesProvider[T] {
 	return i.Provider
 }
 
-func (i *PersitableIterator[T]) Close() {
+func (i *persitableIterator[T]) Close() {
 	close(i.Provider.nextChannel)
 }
 
-func (i *PersitableIterator[T]) store(value T) {
+// Persist a value in the iterator's internal store
+func (i *persitableIterator[T]) store(value T) {
 	i.InternalSlice = append(i.InternalSlice, value)
 }
--- values_provider.go
+++ values_provider.go
@@ -1,19 +1,25 @@
 package laziter
 
+// Produces values and send then through a channel
 type ValuesProvider[T any] struct {
 	valuesChannel chan T
 	nextChannel   chan struct{}
 }
 
+// Wait for next value request
+//
+// If the result of this method is false, the values production should stop and the function calling this method should consider exiting
 func (vp *ValuesProvider[T]) Wait() bool {
 	_, ok := <-vp.nextChannel
 	return ok
 }
 
+// Send a value though a channel
 func (vp *ValuesProvider[T]) Yield(value T) {
 	vp.valuesChannel <- value
 }
 
+// Cleanup values channel
 func (vp *ValuesProvider[T]) Close() {
 	close(vp.valuesChannel)
 }
